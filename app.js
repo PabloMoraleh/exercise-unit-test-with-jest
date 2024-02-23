857a7754e40166aa934f7e18db5a40d39c5127a3
PabloMoraleh <117026954+PabloMoraleh@users.noreply.github.com>
2024-02-23 16:50:38 +0000

    mas simple

--- app.js
+++ app.js
@@ -1,52 +1,27 @@
-// // One euro is:
-// let oneEuroIs = {
-//     "JPY": 156.5, // japan yen
-//     "USD": 1.07, // us dollar
-//     "GBP": 0.87, // british pound
-// }
-// // We declare the function with the exact name "fromEuroToDollar"
-// const fromEuroToDollar = function(oneEuroIs) {
-//     // Convert the given valueInEuro to dollars
-//     let valueInDollar = oneEuroIs * "USD";
-//     // return the dollar value
-//     return valueInDollar;
-// };
-// const fromEuroToYen = function(oneEuroIs) {
-//     // Convert the given valueInEuro to yen
-//     let valueInYen = oneEuroIs * "JPY";
-//     // return the yen value
-//     return valueInYen;
-// };
-// const fromEuroToGBP = function(oneEuroIs) {
-//     // Convert the given valueInEuro to GBP
-//     let valueInGBP = oneEuroIs * "GBP";
-//     // return the GBP value
-//     return valueInGBP;
-// };
-
-// // Export the function to be used on other files
-// // We include fromEuroToDollar here as well because it needs to be exported
-// module.exports = { fromEuroToDollar }
-// // Just a console log for ourselves
-// console.log(fromEuroToDollar(10))
-
-const fromEuroToDollar = function(valueInEuro) {
-    let valueInDollar = valueInEuro * 1.07;
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(amount) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = amount * oneEuroIs["USD"] ;
+    // return the dollar value
     return valueInDollar;
-}
-
-const fromDollarToYen = function(valueInDollar) {
-    let valueInEuro = valueInDollar / 1.07;
-    let valueInYen = valueInEuro * 156.5;
-    valueInYen = Number(valueInYen.toFixed(2))
-    return valueInYen ;
-}
-const fromYenToPound = function(valueInYen) {
-    let valueInEuro = valueInYen / 156.5;
-    let valueInPound = valueInEuro * 0.87;
-    valueInPound = Number(valueInPound.toFixed(2))
-    return valueInPound; 
-}
-console.log(fromYenToPound(1000))
-
-module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound };
+};
+const fromDollarToYen = function(amount) {
+    // Convert the given valueInEuro to dollars
+    let valueInYen = amount / oneEuroIs["USD"] * oneEuroIs["JPY"] ;
+    // return the dollar value
+    return valueInYen;
+};
+const fromYenToPound = function(amount) {
+    // Convert the given valueInEuro to dollars
+    let valueInPound = amount / oneEuroIs["JPY"] * oneEuroIs["GBP"] ;
+    // return the dollar value
+    return valueInPound;
+};
+// const fromDollarToYen = funtion()
+module.exports={fromEuroToDollar, fromDollarToYen, fromYenToPound}
--- test.js
+++ test.js
@@ -13,16 +13,16 @@ test("One euro should be 1.07 dollars", function() {
     // Import the function from app.js
     const { fromEuroToDollar } = require('./app.js');
     expect(fromEuroToDollar(3.5)).toBe(3.745);
-})
+});
 
-test("Conversion from USD to JPY (1 USD = 146.26 JPY)", function() {
+test("One dollar should be 146.26168224299065 yens", function() {
     // Import the function from app.js
-    const { fromDollarToYen } = require('./app.js')
-    expect(fromDollarToYen(10)).toBe(1462.62);
-})
+    const { fromDollarToYen } = require('./app.js');
+    expect(fromDollarToYen(10)).toBe(1462.6168224299065);
+});
 
-test("Conversion from JPY to GBP (1 JPY = 0.0055 GBP)", function() {
+test("One Yen should be 0.0055591054313099035 GBP", function() {
     // Import the function from app.js
-    const { fromYenToPound } = require('./app.js')
-    expect(fromYenToPound(1000)).toBe(5.56);
-})
+    const { fromYenToPound } = require('./app.js');
+    expect(fromYenToPound(1)).toBe(0.0055591054313099035);
+});
